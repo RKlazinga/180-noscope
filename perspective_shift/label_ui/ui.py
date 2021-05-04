28b825a67eaf6771c1309e23a8c51f0320328e6a
RKlazinga <r.f.klazinga@student.tudelft.nl>
2021-05-04 12:19:28 +0200

    Save corrected data and load next

--- perspective_shift/label_ui/ui.py
+++ perspective_shift/label_ui/ui.py
@@ -1,6 +1,10 @@
+import json
+import os
+
 import cv2
-from PyQt5.QtCore import QPoint, QPointF
-from PyQt5.QtGui import QPixmap, QImage
+from PyQt5 import QtGui
+from PyQt5.QtCore import QPoint, QPointF, Qt
+from PyQt5.QtGui import QPixmap, QImage, QKeyEvent
 from PyQt5.QtWidgets import *
 import numpy as np
 
@@ -20,6 +24,8 @@ class UI(QWidget):
     def __init__(self):
         super().__init__()
 
+        self.setWindowTitle("Image Labeler")
+
         self.layout = QGridLayout()
         self.setLayout(self.layout)
 
@@ -30,7 +36,10 @@ class UI(QWidget):
         self.im = ClickablePixmapItem()
         self.im_scene.addItem(self.im)
 
-        self.im_scale = 1
+        self.im_scale = None
+        self.im_path = None
+        self.im_cv_raw = None
+        self.im_warped = None
 
         self.perspective_source = [
             [100, 0],
@@ -47,7 +56,7 @@ class UI(QWidget):
         ])
 
         # add perspective markers
-        self.marker_size = QPixmap("marker.png").size()
+        self.marker_size = QPixmap("perspective_shift/marker.png").size()
         for i in range(4):
             self.im_scene.addItem(PerspectiveMarker(i, self.marker_moved_callback, self.perspective_source[i]))
         self.perspective_source = np.float32(self.perspective_source)
@@ -62,20 +71,51 @@ class UI(QWidget):
         self.control_layout = QVBoxLayout()
         self.layout.addLayout(self.control_layout, 1, 1)
 
-        self.next_btn = QPushButton("Next")
+        self.info_label = QLabel("")
+        self.control_layout.addWidget(self.info_label)
+
+        self.next_btn = QPushButton("Save and load next [SPACEBAR]")
         self.control_layout.addWidget(self.next_btn)
+        self.next_btn.clicked.connect(self.next_image)
 
         self.control_layout.addStretch()
-        self.open_image("01.jpg")
+        self.next_image()
         self.show()
 
+    def next_image(self):
+        # if an image is loaded, save the perspective coords and the output image
+        if self.im_path:
+            self.im_warped.save(f"corrected_data/{self.im_path}")
+            with open(f"corrected_data/{os.path.splitext(self.im_path)[0]}.json", "w") as writefile:
+                writefile.write(json.dumps(self.perspective_source.tolist()))
+
+        new_im = self.get_raw_image()
+        if new_im:
+            self.open_image(new_im)
+
+    def get_raw_image(self):
+        raw_images = set(os.listdir("raw_data"))
+        corrected_images = set(os.listdir("corrected_data"))
+
+        uncorrected_images = raw_images.difference(corrected_images)
+        if len(uncorrected_images) == 0:
+            self.info_label.setText("All images labeled!")
+            return None
+        else:
+            self.info_label.setText(f"{len(uncorrected_images)} of {len(raw_images)} remaining")
+            return uncorrected_images.pop()
+
     def open_image(self, path):
-        pixmap = QPixmap(path)
+        self.im_path = path
+
+        pixmap = QPixmap(f"raw_data/{self.im_path}")
         self.im.setPixmap(pixmap)
 
         scale = max(pixmap.width() / self.IM_WIDTH, pixmap.height() / self.IM_HEIGHT)
         self.im_scale = scale
         self.im.setScale(1 / scale)
+
+        self.im_cv_raw = cv2.imread(f"raw_data/{self.im_path}")
 
     def marker_moved_callback(self, marker_idx, pos: QPointF):
         self.perspective_source[marker_idx][0] = (pos.x() + self.marker_size.width()//2) * self.im_scale
@@ -86,11 +126,15 @@ class UI(QWidget):
     def recompute_warped(self):
         matrix = cv2.getPerspectiveTransform(self.perspective_source,
                                              self.perspective_target)
-        im = cv2.imread("01.jpg")
-        result = cv2.warpPerspective(im, matrix, (size, size))
+
+        result = cv2.warpPerspective(self.im_cv_raw, matrix, (size, size))
         height, width, channel = result.shape
-        q_image = QImage(result.data, width, height, 3 * width, QImage.Format_BGR888)
-        self.sample_output.setPixmap(QPixmap(q_image))
+        self.im_warped = QImage(result.data, width, height, 3 * width, QImage.Format_BGR888)
+        self.sample_output.setPixmap(QPixmap(self.im_warped))
+
+    def keyPressEvent(self, event: QKeyEvent):
+        if event.key() == Qt.Key_Space:
+            self.next_image()
 
 
 class ClickablePixmapItem(QGraphicsPixmapItem):
@@ -98,7 +142,7 @@ class ClickablePixmapItem(QGraphicsPixmapItem):
         super().__init__()
 
     def mousePressEvent(self, event: QGraphicsSceneMouseEvent):
-        print(event.pos())
+        pass
 
 
 class PerspectiveMarker(QGraphicsPixmapItem):
@@ -108,8 +152,7 @@ class PerspectiveMarker(QGraphicsPixmapItem):
         self.callback = callback
 
         self.setPos(*pos)
-        # TODO improve marker PNG
-        self.setPixmap(QPixmap("marker.png"))
+        self.setPixmap(QPixmap("perspective_shift/marker.png"))
 
     def mousePressEvent(self, event):
         pass
