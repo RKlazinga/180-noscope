557727c3aa7112d3f742a17be57a5ea26d0fce36
RKlazinga <r.f.klazinga@student.tudelft.nl>
2021-05-20 01:40:46 +0200

    Train first network

--- perspective_shift/network/network.py
+++ perspective_shift/network/network.py
@@ -1,9 +1,15 @@
 import os
 
 import torch
+import torchsummary
+from torch.nn import MSELoss
+from torch.utils.data import DataLoader
 from torchvision import transforms
 from PIL import Image
-from torch import nn
+from torch import nn, optim
+from tqdm import tqdm
+
+from perspective_shift.network.dataset import PerspectiveDataset
 
 
 class ConvUnit(nn.Module):
@@ -29,9 +35,9 @@ class PerspectiveNetwork(nn.Module):
     def __init__(self):
         super().__init__()
 
-        mult = 16
+        mult = 6
         in_size = 256
-        conv_layers = 6
+        conv_layers = 7
         regression_out = 8
 
         self.layers = [
@@ -58,8 +64,43 @@ class PerspectiveNetwork(nn.Module):
 
 
 if __name__ == '__main__':
-    network = PerspectiveNetwork()
 
-    single_im = Image.open("../augmented_data/IMG_20210511_142817727-1f47bf.jpg")
-    single = transforms.ToTensor()(single_im).view(1, 3, 256, 256)
-    print(network.forward(single))
+    network = PerspectiveNetwork()
+    optimiser = optim.Adam(network.parameters())
+    torchsummary.summary(network, input_size=(3, 256, 256))
+
+    train_dataset = PerspectiveDataset()
+    train_loader = DataLoader(train_dataset, batch_size=32)
+
+    test_dataset = PerspectiveDataset(is_test=True)
+    test_loader = DataLoader(test_dataset, batch_size=32)
+
+    criterion = MSELoss()
+
+    for epoch in range(10):
+        train_loss = 0
+        for batch in tqdm(train_loader):
+            optimiser.zero_grad()
+
+            out = network.forward(batch["input"])
+
+            single_loss = criterion(out, batch["label"])
+
+            single_loss.backward()
+            optimiser.step()
+
+            train_loss += single_loss.item()
+        train_loss /= len(train_loader)
+
+        test_loss = 0
+        with torch.no_grad():
+            for batch in tqdm(test_loader):
+                out = network.forward(batch["input"])
+                single_loss = criterion(out, batch["label"])
+                test_loss += single_loss.item()
+        test_loss /= len(test_loader)
+
+        print(f"Epoch {epoch}\t Train Loss={train_loss}\t Test Loss={test_loss}")
+    torch.save(network.state_dict(), os.path.join(os.getcwd(),"NETWORK.pth"))
+
+
--- perspective_shift/network/run.py
+++
@@ -1,5 +0,0 @@
-import torch
-
-if __name__ == '__main__':
-    device = torch.device("cpu")
-
