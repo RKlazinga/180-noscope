827a3688086b105ce50c0843dd1386f5e3d21aea
Simon Tulling <simon.tulling99@gmail.com>
2021-06-11 08:25:05 +0200

    multiple darts

--- data_collection/dartboard_augmentation/generate_images.py
+++ data_collection/dartboard_augmentation/generate_images.py
@@ -78,19 +78,46 @@ def generate(num_images: int):
 
     for i in tqdm(range(num_images)):
         board = random.choice(boards).copy()
-        dart = random.choice(darts)
-        position, square = random_position(board)
-        board.paste(dart, (int(position[0]) - dart.width // 2, int(position[1]) - dart.height // 2), dart)
+        im = Image.new('RGBA', board.size)
+        positions = []
+        squares = []
+        for _ in range(3):
+            dart = random.choice(darts)
+            position, square = random_position(board)
+            positions.append(position)
+            squares.append(square)
+
+            persp = np.asarray([[0, 0], [0, dart.size[1]], [dart.size[0], dart.size[1]], [dart.size[0], 0]], np.float32)
+            # warp perspective
+            # basic way: add gaussian noise to the 4 corner points
+            warped_persp = persp.copy()
+            for i in range(4):
+                for j in range(2):
+                    v = warped_persp[i][j]
+                    v += random.gauss(0, 20)
+                    # ensure none of the perspective points will fall outside the cropped image
+                    # v = max(box[j] + 5, v)
+                    # v = min(box[j + 2] - 5, v)
+                    warped_persp[i][j] = v
+
+            im_cv = cv2.cvtColor(np.array(dart), cv2.COLOR_RGBA2BGRA)
+            matrix = cv2.getPerspectiveTransform(persp, warped_persp)
+            warped_im = cv2.warpPerspective(im_cv, matrix, (dart.width, dart.height))
+            warped_dart = Image.fromarray(cv2.cvtColor(warped_im, cv2.COLOR_BGRA2RGBA))
+
+            im.paste(dart, (int(position[0]) - dart.width // 2, int(position[1]) - dart.height // 2), dart)
+
+        board.paste(im, (0, 0), im)
         #board.show()
         fname = f"{hex(random.randint(2**20, 2**24))[2:]}"
         board.save(f"../../data/generated/{fname}.jpg")
         with open(f"../../data/generated/{fname}.json", "w") as write_file:
             data = {
-                "square": square,
-                "position": position
+                "square": squares,
+                "position": positions
             }
             write_file.write(json.dumps(data))
 
 
 if __name__ == '__main__':
-    generate(10)
+    generate(5)
